3d8bbd7ce4a7a7c2ca2b70ae92b900b76baad7da
PALAK AGRAWAL <agrawalpalak6666@gmail.com>
2026-08-25 20:29:33 +0530

    Add: permutation-string - submission-0

---
+++ Data Structures & Algorithms/permutation-string/submission-0.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool checkInclusion(string s1, string s2) {
+        sort(s1.begin(),s1.end());
+        int n1 = s1.size();
+        int n2 = s2.size();
+
+        if(n1>n2) return false;
+        if(s1==s2) return true;
+
+        int s = 0;
+        int e = n1;
+
+        while(s<=(n2-n1)){
+            string sub = s2.substr(s,e);
+            sort(sub.begin(),sub.end());
+            if(sub == s1){
+                return true;
+            }
+            s++;
+        }
+
+        return false;
+    }
+};
